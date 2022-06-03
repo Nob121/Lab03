58cf17a074267cbc6216ca1e4328f595ca976fff
Arnob <tahmidambrose.420@gmail.com>
2022-06-03 11:15:16 -0700

    Logic error for addition is fixed

--- src/calculator125/Calculator125.java
+++ src/calculator125/Calculator125.java
@@ -9,8 +9,7 @@ public class Calculator125 {
     public static void main(String[] args) {
         CalculatorView theView = new CalculatorView();        
     	CalculatorModel theModel = new CalculatorModel();       
-        CalculatorController theController = 
-                new CalculatorController(theView,theModel);        
+        CalculatorController theController = new CalculatorController(theView,theModel);        
         theView.setVisible(true);
     }
     
